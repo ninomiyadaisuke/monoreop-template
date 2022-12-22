7701644e917305d2d48e2f45568f6f4bd37a5356
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-22 11:12:47 +0900

    called Button component

--- apps/app-one/pages/index.tsx
+++ apps/app-one/pages/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { Button } from "ui";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h2>monorepo Template</h2>
+      <Button/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
